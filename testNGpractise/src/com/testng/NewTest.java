6503dfb856ef21c73a52d5b927738c1883a80b34
ajain388 <ajain388@IN5CD6335WYY.CSCMWS.CSCMWS.COM>
2019-08-01 19:28:39 +0530

    Added Project- TestNgpractise

---
+++ testNGpractise/src/com/testng/NewTest.java
@@ -0,0 +1,17 @@
+package com.testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test(dependsOnMethods={"f1"})
+  public void f() {
+	Reporter.log("Test1");  
+  
+  }
+  @Test
+  public void f1() {
+	Reporter.log("Test");  
+  
+  }
+}
